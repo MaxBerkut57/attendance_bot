3cb15647da26bcf34042bf65b540846959e2fb60
Emil <panteon507@gmail.com>
2026-05-01 08:50:16 +0300

    remastered main

--- bot/main.py
+++ bot/main.py
@@ -1,19 +1,26 @@
 import asyncio
 import aiohttp
 from aiohttp_socks import ProxyConnector
-
 from aiogram import Bot, Dispatcher
-
+from aiogram.client.session.aiohttp import AiohttpSession
 from bot.config import settings
 from bot.logger import logger
 from bot.db.database import create_tables, engine
+
+class ProxiedAiohttpSession(AiohttpSession):
+    def __init__(self, proxy_url: str, **kwargs):
+        self._proxy_url = proxy_url
+        super().__init__(**kwargs)
+
+    async def create_session(self, **kwargs):
+        connector = ProxyConnector.from_url(self._proxy_url)
+        return aiohttp.ClientSession(connector=connector, **kwargs)
 
 async def main():
     await create_tables()
 
     if settings.PROXY_URL:
-        connector = ProxyConnector.from_url(settings.PROXY_URL)
-        session = aiohttp.ClientSession(connector=connector)
+        session = ProxiedAiohttpSession(proxy_url=settings.PROXY_URL)
         bot = Bot(token=settings.BOT_TOKEN, session=session)
         logger.info("Using SOCKS5 proxy for Telegram API")
     else:
